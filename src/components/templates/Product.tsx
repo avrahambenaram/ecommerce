19bdece8d677bdc51e2e9cb3b81a3637fa526999
Avraham <avrahambenaramsouza@gmail.com>
2023-04-12 20:55:42 -0300

    Meta Description updated

--- src/components/templates/Product.tsx
+++ src/components/templates/Product.tsx
@@ -12,7 +12,7 @@ export function ProductTemplate({ product }: ProductTemplateProps) {
     <>
       <Head>
         <title>{product.name}</title>
-        <meta name="description" content="Ecommerce landing page" />
+        <meta name="description" content={product.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Header/>
